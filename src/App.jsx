f821b105bb3cc7aed65da226b2aecdde0218c480
DB3NJ4 <150455520+DB3NJ4@users.noreply.github.com>
2025-11-10 18:33:10 -0300

    se añaden imagenes de smartwniery

--- src/App.jsx
+++ src/App.jsx
@@ -50,39 +50,24 @@ const products = [
     thumbnail: "/reserva_agenda.png",
   },
   {
-    title: "Tailwind Master Kit",
-    link: "https://tailwindmasterkit.com",
-    thumbnail: "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+    title: "SmartWinery PowerBI",
+    link: "",
+    thumbnail: "/smartwinery_multiple.png",
   },
   {
-    title: "SmartBridge",
-    link: "https://smartbridgetech.com",
-    thumbnail: "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+    title: "SmartWinery PowerBI",
+    link: "",
+    thumbnail: "/smartwinery_home.png",
   },
   {
-    title: "Renderwork Studio",
-    link: "https://renderwork.studio",
-    thumbnail: "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+    title: "SmartWinery PowerBI",
+    link: "",
+    thumbnail: "/sw_nitrogeno.png",
   },
   {
-    title: "Creme Digital",
-    link: "https://cremedigital.com",
-    thumbnail: "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-  },
-  {
-    title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
-    thumbnail: "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-  },
-  {
-    title: "Invoker Labs",
-    link: "https://invoker.lol",
-    thumbnail: "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-  },
-  {
-    title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
-    thumbnail: "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+    title: "SmartWinery PowerBI",
+    link: "",
+    thumbnail: "/smartwinery_multiple.png",
   },
 ]
 
